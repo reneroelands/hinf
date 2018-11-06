79f9b9a326c7d729b0aaee8eb10f81cb04915f78
rene <rene.roelands@gmail.com>
2018-11-06 19:44:51 +0100

    small changes

--- lqg2.py
+++ lqg2.py
@@ -8,27 +8,27 @@ This is a temporary script file.
 from control import ss, ss2tf
 from control import bode, nyquist, step_response
 import scipy.linalg as linalg
-from numpy import matrix
+from numpy import matrix, eye
 import numpy as np
 from numpy.linalg import inv
 import matplotlib.pyplot as plt
 #-----------------------
-
+# attempt to create controller with integrator
 m = 200.0
-A = matrix([[0.0, 1.0], [0.0, 0.0]])
-B = matrix([[0.0], [1/m]])
-C = matrix([[1.0, 0.0]])
+A = matrix([[0.0, 1.0, 0.0], [0.0, 0.0, 1.0], [0.0, 0.0, 0.0]])
+B = matrix([[0.0], [0.0],  [1/m]])
+C = matrix([[1.0, 0.0, 0.0]])
 D = matrix([[0.0]])
 G = ss(A, B, C, D)
 P = ss2tf(G)
 #------
 W = matrix([[1.0]])
-V = 1e8*matrix([[1.0, 0.0], [0.0, 1.0]])
+V = 1e8*eye(3)
 Y = linalg.solve_continuous_are(A.transpose(), C.transpose(), V, W)
 L = Y*C.transpose()*inv(W)
 #-------------------
 R = matrix([[1.0]])
-Q = matrix([[1e2, 0.0], [0.0, 1.]])
+Q = matrix([[1.0, 0.0, 0.0], [0.0, 1e2, 0.0], [0.0, 0.0, 1.]])
 X = linalg.solve_continuous_are(A, B, Q, R)
 K = inv(R)*B.transpose()*X
 # synthesis, controller in state space
